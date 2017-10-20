3e8c214a25cd40374f16f41fa68aacc2c3a2d9f1
Sreeraj <Sreeraj@disroot.org>
2017-10-20 14:12:38 +0000

    rm

---
+++ rm.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void main(int argc, char *argv[])
+{
+	if(argc != 2) 
+	{
+		printf("Wrong format");
+		return ;
+	}
+
+	if( access( argv[1], F_OK ) != -1 )
+	{
+		if(unlink(argv[1])!=0)
+		{
+			printf("error");
+		}
+	}
+	else
+	{
+		printf("error, no such file");
+	}
+	
+	
+}
